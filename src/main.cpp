92495fb1831dd6076a1bdd6953e4111a85073bb1
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2018-01-29 14:40:25 +0100

    error removal

--- src/main.cpp
+++ src/main.cpp
@@ -248,8 +248,8 @@ int main() {
 			double dist_inc = 0.5;
 			for (int i = 0l i < 50; i++)
 			{
-				next_x_vals.push_back(car_x + (dist_inc*i) * cos(deg2rad(car_yaw));
-				next_y_vals.push_back(car_y + (dist_inc*i) * sin(deg2rad(car_yaw));
+				next_x_vals.push_back(car_x + (dist_inc*i) * cos(deg2rad(car_yaw)));
+				next_y_vals.push_back(car_y + (dist_inc*i) * sin(deg2rad(car_yaw)));
 			}
 
 			msgJson["next_x"] = next_x_vals;
