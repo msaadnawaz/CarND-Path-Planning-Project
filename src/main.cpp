f4644c20290085fd22e09222bc1292189d92ba86
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2018-02-03 11:23:48 +0100

    behavior planning trial

--- src/main.cpp
+++ src/main.cpp
@@ -179,8 +179,10 @@ int main() {
   // The max s value before wrapping around the track back to 0
   double max_s = 6945.554;
 
-  //start in middle lane || 2 | 1 | 0 ||
+  //start in middle lane || 0 | 1 | 2 ||
   int lane = 1;
+
+  int goal_lane = lane;
 
   //reference velocity
   double ref_vel = 0; //in mph
@@ -209,7 +211,7 @@ int main() {
   }
 
 
-  h.onMessage([&ref_vel, &lane, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&ref_vel, &lane, &goal_lane, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -245,15 +247,17 @@ int main() {
 
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
-			
+
+			///////////////////////////////////////////BEHAVIOR PLANNING//////////////////////////////////////////////////
 			int prev_size = previous_path_x.size();
 
 			if (prev_size > 0)
 			{
 				car_s = end_path_s;
 			}
-
+			
 			bool too_close = false;
+			bool car_in_danger_zone = false;
 
 			for (int i = 0; i < sensor_fusion.size(); i++)
 			{
@@ -270,20 +274,61 @@ int main() {
 					if ((check_car_s > car_s) && ((check_car_s - car_s) < 30))
 					{
 						too_close = true;
-						if (lane == 1)
-							lane = 0;
-						else if (lane == 2)
-							lane = 1;
+					}
+				}
+
+				if (goal_lane != lane)
+				{
+					if (d < (2 + 4 * goal_lane + 2) && d >(2 + 4 * goal_lane - 2))
+					{
+						double vx = sensor_fusion[i][3];
+						double vy = sensor_fusion[i][4];
+						double check_speed = sqrt(vx * vx + vy * vy);
+						double check_car_s = sensor_fusion[i][5];
+
+						check_car_s += (double)prev_size * 0.02 * check_speed;
+
+						if (abs(check_car_s - car_s) < 30)
+						{
+							car_in_danger_zone = true;
+						}
 					}
 				}
 				
 			}
 
 			if (too_close)
+			{
 				ref_vel -= 0.224;
+				if (goal_lane == lane)
+				{
+					if (lane > 0)
+						goal_lane = lane - 1;
+					else
+						goal_lane = lane + 1;
+				}
+				else
+				{
+					if (car_in_danger_zone)
+					{
+						if (goal_lane == 0)
+							goal_lane = 2;
+						else if ((goal_lane == 2) || (goal_lane == 1))
+							goal_lane = lane;
+					}
+					else
+						lane = goal_lane;
+				}
+			}
+
 			else if(ref_vel < 49.5)
 				ref_vel += 0.224;
 
+			
+
+
+			///////////////////////////////////////////TRAJECTORY GENERATION//////////////////////////////////////////////////
+			// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
 			vector<double> ptsx;
 			vector<double> ptsy;
 
@@ -380,8 +425,9 @@ int main() {
 				next_x_vals.push_back(x_point);
 				next_y_vals.push_back(y_point);
 			}
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
 
+			////////////////////////////////////////////////////////////////////////////////////////////////////
+         
 			json msgJson;
 
 			msgJson["next_x"] = next_x_vals;
